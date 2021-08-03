894b03c510f21b04db4db5873b28e4886a1dd0c7
Robert Davidson <rdavidson@accusoft.com>
2021-08-02 21:24:28 -0400

    Fix the publish script (for real this time)

--- name_generator_web/publish_iis.ps1
+++ name_generator_web/publish_iis.ps1
@@ -2,10 +2,11 @@ $ErrorActionPreference = "Stop"
 dotnet publish --configuration Release
 Compress-Archive -Path "bin\Release\net5.0\publish\*" -DestinationPath "name_generator.zip" -Update
 $remoteSession = New-Ec2Session.ps1
-# Invoke-Command -Session $remoteSession { iisreset.exe /stop }
-Invoke-Command -Session $remoteSession { Stop-IISSite "name_generator" }
-Copy-Item -Recurse -Force -Path "name_generator.zip" -ToSession $remoteSession -Destination "C:\Users\Administrator\Desktop\name_generator.zip"
-Invoke-Command -Session $remoteSession { Expand-Archive -LiteralPath "C:\Users\Administrator\Desktop\name_generator.zip" -DestinationPath "C:\Users\Administrator\Desktop\Sitepath"  }
-# Invoke-Command -Session $remoteSession { iisreset.exe /start }
-Invoke-Command -Session $remoteSession { Start-IISSite "name_generator" }
-Remove-PSSession $remoteSession
+Try {
+    Copy-Item -Recurse -Force -Path "name_generator.zip" -ToSession $remoteSession -Destination "C:\Users\Administrator\Desktop\name_generator.zip"
+    Invoke-Command -Session $remoteSession { iisreset.exe /stop }
+    Invoke-Command -Session $remoteSession { Expand-Archive -Force -LiteralPath "C:\Users\Administrator\Desktop\name_generator.zip" -DestinationPath "C:\Users\Administrator\Desktop\Sitepath"  }
+    Invoke-Command -Session $remoteSession { iisreset.exe /start }
+} Finally {
+    Remove-PSSession $remoteSession
+}
