22777386873645f6aa8497965e23303aa85be4ab
Robert Davidson <rdavidson@accusoft.com>
2021-08-03 21:12:55 -0400

    Fix the publish script (for real this time)

--- name_generator_web/publish_iis.ps1
+++ name_generator_web/publish_iis.ps1
@@ -4,9 +4,12 @@ Compress-Archive -Path "bin\Release\net5.0\publish\*" -DestinationPath "name_gen
 $remoteSession = New-Ec2Session.ps1
 Try {
     Copy-Item -Recurse -Force -Path "name_generator.zip" -ToSession $remoteSession -Destination "C:\Users\Administrator\Desktop\name_generator.zip"
-    Invoke-Command -Session $remoteSession { iisreset.exe /stop }
-    Invoke-Command -Session $remoteSession { Expand-Archive -Force -LiteralPath "C:\Users\Administrator\Desktop\name_generator.zip" -DestinationPath "C:\Users\Administrator\Desktop\Sitepath"  }
-    Invoke-Command -Session $remoteSession { iisreset.exe /start }
+    Invoke-Command -Session $remoteSession { 
+        iisreset.exe /stop
+        Remove-Item -Recurse -Force "C:\Users\Administrator\Desktop\Sitepath\*"
+        Expand-Archive -Force -LiteralPath "C:\Users\Administrator\Desktop\name_generator.zip" -DestinationPath "C:\Users\Administrator\Desktop\Sitepath"
+        iisreset.exe /start 
+    }
 } Finally {
     Remove-PSSession $remoteSession
 }
